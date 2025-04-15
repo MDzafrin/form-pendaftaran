04377dd25ed479f748405d4eb334da624b307531
Hype <silviikmahprasista@gmail.com>
2025-04-15 16:37:10 +0700

    menambahkan fungsi nama, jenis kelamin, prodi, tingkat dan email pada file create

--- resources/views/forms/create.blade.php
+++ resources/views/forms/create.blade.php
@@ -2,6 +2,58 @@
 
 @section('content')
 
-<h1>Create Form..</h1>
+<h1>FORMULIR PENDAFTARAN</h1>
+
+<!--Fungsi Nama-->
+<form>
+    <div class="mb-3">
+        <legend for="nama-lengkap" class="form-label">Nama Lengkap</legend>
+        <input type="nama" class="form-control" id="nama-lengkap" aria-describeby="TolongNama">
+    </div>
+
+    <!--Fungsi Jenis Kelamin-->
+    <form action="/action_page.php">
+    <fieldset>    
+    <legend for="jenis-kelamin" class="form-label">Jenis Kelamin</legend>
+    <div class="form-check">
+        <label for="radioDefault1"><input id="laki-laki" type="radio" name="radio" class="inline" checked/>Laki-Laki</label>
+    </div>
+
+    <div class="form-check">
+        <label for="radioDefault2"><input id="perempuan" type="radio" name="radio" class="inline" />Perempuan</label>
+    </div>
+    </fieldset>
+    </form>
+
+    <!--Fungsi Pemilihan Prodi Dan Tingkat-->
+    <fieldset>
+    <legend for="tingkat-dan-prodi" class="form-label">Pilih Tingkat Dan Prodi </legend>
+    <select class="form-select form-select-lg mb-3" aria-label="Large select example">
+        <option selected>Pilih Prodi</option>
+        <option value="1">Manajemen Industri</option>
+        <option value="2">Teknologi Rekayasa Mekatronika</option>
+        <option value="3">Teknik Mesin Industri</option>
+    </select>
+
+    <select class="form-select form-select-sm" aria-label="Small select example">
+        <option selected>Pilih Tingkat</option>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+    </select>
+    </fieldset>
+
+    
+    <fieldset>
+        <div class="mb-3">
+            <legend for="exampleFormControlInput1" class="form-label">Email address</legend>
+            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="nama@example.com">
+        </div>
+    </fieldset>
+    
+
+</form>
+
+
 
 @endsection
--- resources/views/forms/layout.blade.php
+++ resources/views/forms/layout.blade.php
@@ -1,17 +1,20 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Formulir Pendaftaran Manufaktur Competition</title>
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.css" rel="stylesheet">
+        <title>MANUFAKTUR COMPETITION</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet">
     </head>
 
 <body>
-    <h1>Formulir Pendaftaran Manufaktur Competition</h1>
+    <h1>EVENT MANUFAKTUR COMPETITION</h1>
 
     <div class="container">
         <br>
         @yield('content')
     </div>
+
+
+    
 
 </body>
 </html>
