8285c89a586ad08873527dab59ef82e14500631c
yeniirahmaa512 <yenirahma396@gmail.com>
2025-05-28 10:32:44 +0700

    local bootstrap

--- resources/views/forms/layout.blade.php
+++ resources/views/forms/layout.blade.php
@@ -4,7 +4,8 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>MANUFAKTUR COMPETITION</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" />
+  {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" /> --}}
+  <link rel="stylesheet" href="{{ URL::asset('assets/css/bootstrap.min.css')}}">
   <link rel="stylesheet" href="{{ URL::asset('assets/css/styles.css')}}">
  
 </head>
